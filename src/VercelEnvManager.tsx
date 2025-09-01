d376ac3bef8f3cec8b2bab8f45ef2f6a8c4e44b8
rajveer9720 <rajveersingh05021999@gmail.com>
2025-09-01 20:49:07 +0530

    Extra remove code

--- src/VercelEnvManager.tsx
+++ src/VercelEnvManager.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getEnvVars, addEnvVar, updateEnvVar, deleteEnvVar } from './vercelApi';
 
 interface EnvVar {
